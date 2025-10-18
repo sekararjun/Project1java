59984be55c19f9f59f64cec2a3271d8c9c688110
sekararjun <113541739+sekararjun@users.noreply.github.com>
2025-10-18 10:31:23 +0530

    Create greatest of 3.java

---
+++ greatest of 3.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=12;
+	    int b=24;
+	    int c=13;
+		if((a>b)&(a>c)){
+		    System.out.println("A is greater");
+		}else if(b>c){
+		    System.out.println("B is greater");
+		}
+		else{
+		    System.out.println("C is greater");
+		}
+	}
+}
